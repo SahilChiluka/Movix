c64286c5195dcdb3d4ea07df24407dc4d210a08e
sahil <sahilchiluka10@gmail.com>
2023-08-09 21:54:06 +0530

    error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Home from './pages/home/Home';
 import Details from './pages/details/Details';
 import SearchResult from './pages/searchResult/SearchResult';
 import Explore from './pages/explore/Explore';
-import PageNotFound from './pages/404/pageNotFound';
+
 
 function App() {
   const dispatch = useDispatch();
@@ -61,7 +61,6 @@ function App() {
         <Route path="/:mediaType/:id" element={<Details />} />
         <Route path="/search/:query" element={<SearchResult />} />
         <Route path="/explore/:mediaType" element={<Explore />} />
-        <Route path="*" element={<PageNotFound />} />
       </Routes>
      <Footer />
     </BrowserRouter>
